9db07e7b1f9691bde7b58bac62bdea8bbfdb6228
Tariq <tariqhirji15@gmail.com>
2019-03-27 19:55:46 -0400

    Deleted Comments

--- baseEntities/IEntity.java
+++ baseEntities/IEntity.java
@@ -1,17 +1,5 @@
 package baseEntities;
 
-<<<<<<< HEAD
-// This is my fist change in github
-// I will now send a push request
-// Someone must pull this and edit it to merge it 
-
-//Savio is making a change cause he wants to :)
-//Faiez change
-//Arshia Changes
-
-//Chnage from intellij
-=======
->>>>>>> parent of 69abc90... Update IEntity.java
 
 //Change two 
  /**
