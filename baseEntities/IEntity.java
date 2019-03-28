382becb39dbcc82d1ddc05e5b9750cf27311e385
falkhour <falkhour@uwo.ca>
2019-03-27 20:11:42 -0400

    updated Ientity

--- baseEntities/IEntity.java
+++ baseEntities/IEntity.java
@@ -4,10 +4,12 @@ package baseEntities;
 //Change two 
  /**
   * @author kkontog, ktsiouni, mgrigori
-  *  
+  *
   * Base Interface for Publisher and Subscriber classes
   */
 public interface IEntity{
+
+ //Faiez just change this
 
 
 }
